55ec410dbf79485f992dc1f2e07f736526d96abc
P1943034572 <jan.dierickx@student.ehb.be>
2018-12-03 10:53:53 +0000

    initial commit

---
+++ webapp/controller/DetailEmployees.controller.js
@@ -0,0 +1,107 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.project3.controller.DetailEmployees", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5bfbee02a519720ccef17e57";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype") {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onButtonPress: function() {
+
+			var sDialogName = "";
+			this.mDialogs = this.mDialogs || {};
+			var oDialog = this.mDialogs[sDialogName];
+
+			if (!oDialog) {
+				oDialog = new(this.getView());
+				this.mDialogs[sDialogName] = oDialog;
+
+				// For navigation.
+				oDialog.setRouter(this.oRouter);
+			}
+			oDialog.open();
+
+		},
+		_onButtonPress1: function(oEvent) {
+
+			var oSource = oEvent.getSource();
+			var oSourceBindingContext = oSource.getBindingContext();
+
+			return new Promise(function(fnResolve, fnReject) {
+				if (oSourceBindingContext) {
+					var oModel = oSourceBindingContext.getModel();
+					oModel.remove(oSourceBindingContext.getPath(), {
+						success: function() {
+							oModel.refresh();
+							fnResolve();
+						},
+						error: function() {
+							oModel.refresh();
+							fnReject(new Error("remove failed"));
+						}
+					});
+				}
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("DetailPage1").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function() {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function() {
+								this.oRouter.navTo("MasterPage1", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+		}
+	});
+}, /* bExport= */ true);
