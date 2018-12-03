05f5a0046d47a210efec150ecbc54af91355940f
P1943034572 <jan.dierickx@student.ehb.be>
2018-12-03 18:34:36 +0000

    initial commit

---
+++ webapp/controller/DetailReport.controller.js
@@ -0,0 +1,208 @@
+sap.ui.define(["sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(BaseController, MessageBox, Utilities, History) {
+	"use strict";
+
+	return BaseController.extend("com.sap.build.standard.project3.controller.DetailReport", {
+		handleRouteMatched: function(oEvent) {
+			var sAppId = "App5bfbee02a519720ccef17e57";
+
+			var oParams = {};
+
+			if (oEvent.mParameters.data.context) {
+				this.sContext = oEvent.mParameters.data.context;
+
+			} else {
+				if (this.getOwnerComponent().getComponentData()) {
+					var patternConvert = function(oParam) {
+						if (Object.keys(oParam).length !== 0) {
+							for (var prop in oParam) {
+								if (prop !== "sourcePrototype") {
+									return prop + "(" + oParam[prop][0] + ")";
+								}
+							}
+						}
+					};
+
+					this.sContext = patternConvert(this.getOwnerComponent().getComponentData().startupParameters);
+
+				}
+			}
+
+			var oPath;
+
+			if (this.sContext) {
+				oPath = {
+					path: "/" + this.sContext,
+					parameters: oParams
+				};
+				this.getView().bindObject(oPath);
+			}
+
+		},
+		_onPageNavButtonPress: function() {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+			var oQueryParams = this.getQueryParameters(window.location);
+
+			if (sPreviousHash !== undefined || oQueryParams.navBackToLaunchpad) {
+				window.history.go(-1);
+			} else {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("default", true);
+			}
+
+		},
+		getQueryParameters: function(oLocation) {
+			var oQuery = {};
+			var aParams = oLocation.search.substring(1).split("&");
+			for (var i = 0; i < aParams.length; i++) {
+				var aPair = aParams[i].split("=");
+				oQuery[aPair[0]] = decodeURIComponent(aPair[1]);
+			}
+			return oQuery;
+
+		},
+		applyFiltersAndSorters: function(sControlId, sAggregationName, chartBindingInfo) {
+			if (chartBindingInfo) {
+				var oBindingInfo = chartBindingInfo;
+			} else {
+				var oBindingInfo = this.getView().byId(sControlId).getBindingInfo(sAggregationName);
+			}
+			var oBindingOptions = this.updateBindingOptions(sControlId);
+			this.getView().byId(sControlId).bindAggregation(sAggregationName, {
+				model: oBindingInfo.model,
+				path: oBindingInfo.path,
+				parameters: oBindingInfo.parameters,
+				template: oBindingInfo.template,
+				templateShareable: true,
+				sorter: oBindingOptions.sorters,
+				filters: oBindingOptions.filters
+			});
+
+		},
+		updateBindingOptions: function(sCollectionId, oBindingData, sSourceId) {
+			this.mBindingOptions = this.mBindingOptions || {};
+			this.mBindingOptions[sCollectionId] = this.mBindingOptions[sCollectionId] || {};
+
+			var aSorters = this.mBindingOptions[sCollectionId].sorters;
+			var aGroupby = this.mBindingOptions[sCollectionId].groupby;
+
+			// If there is no oBindingData parameter, we just need the processed filters and sorters from this function
+			if (oBindingData) {
+				if (oBindingData.sorters) {
+					aSorters = oBindingData.sorters;
+				}
+				if (oBindingData.groupby || oBindingData.groupby === null) {
+					aGroupby = oBindingData.groupby;
+				}
+				// 1) Update the filters map for the given collection and source
+				this.mBindingOptions[sCollectionId].sorters = aSorters;
+				this.mBindingOptions[sCollectionId].groupby = aGroupby;
+				this.mBindingOptions[sCollectionId].filters = this.mBindingOptions[sCollectionId].filters || {};
+				this.mBindingOptions[sCollectionId].filters[sSourceId] = oBindingData.filters || [];
+			}
+
+			// 2) Reapply all the filters and sorters
+			var aFilters = [];
+			for (var key in this.mBindingOptions[sCollectionId].filters) {
+				aFilters = aFilters.concat(this.mBindingOptions[sCollectionId].filters[key]);
+			}
+
+			// Add the groupby first in the sorters array
+			if (aGroupby) {
+				aSorters = aSorters ? aGroupby.concat(aSorters) : aGroupby;
+			}
+
+			var aFinalFilters = aFilters.length > 0 ? [new sap.ui.model.Filter(aFilters, true)] : undefined;
+			return {
+				filters: aFinalFilters,
+				sorters: aSorters
+			};
+
+		},
+		onInit: function() {
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter.getTarget("DetailReport").attachDisplay(jQuery.proxy(this.handleRouteMatched, this));
+			var oView = this.getView();
+			oView.addEventDelegate({
+				onBeforeShow: function() {
+					if (sap.ui.Device.system.phone) {
+						var oPage = oView.getContent()[0];
+						if (oPage.getShowNavButton && !oPage.getShowNavButton()) {
+							oPage.setShowNavButton(true);
+							oPage.attachNavButtonPress(function() {
+								this.oRouter.navTo("MasterPage", {}, true);
+							}.bind(this));
+						}
+					}
+				}.bind(this)
+			});
+
+			var oView = this.getView(),
+				oData = {},
+				self = this;
+			var oModel = new sap.ui.model.json.JSONModel();
+			oView.setModel(oModel, "staticDataModel");
+			self.oBindingParameters = {};
+
+			oData["sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545"] = {};
+
+			oData["sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545"]["data"] = [{
+				"dim0": "Teamplayer",
+				"mea0": "7",
+				"__id": 0
+			}, {
+				"dim0": "Attendance",
+				"mea0": "9",
+				"__id": 1
+			}, {
+				"dim0": "Workload",
+				"mea0": "5",
+				"__id": 2
+			}, {
+				"dim0": "Helpfull",
+				"mea0": "6",
+				"__id": 3
+			}];
+
+			self.oBindingParameters['sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545'] = {
+				"path": "/sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545/data",
+				"model": "staticDataModel",
+				"parameters": {}
+			};
+
+			oView.getModel("staticDataModel").setData(oData, true);
+
+			function dateDimensionFormatter(oDimensionValue, sTextValue) {
+				var oValueToFormat = sTextValue !== undefined ? sTextValue : oDimensionValue;
+				if (oValueToFormat instanceof Date) {
+					var oFormat = sap.ui.core.format.DateFormat.getDateInstance({
+						style: "short"
+					});
+					return oFormat.format(oValueToFormat);
+				}
+				return oValueToFormat;
+			}
+
+			var aDimensions = oView.byId("sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545").getDimensions();
+			aDimensions.forEach(function(oDimension) {
+				oDimension.setTextFormatter(dateDimensionFormatter);
+			});
+
+		},
+		onAfterRendering: function() {
+
+			var oChart,
+				self = this,
+				oBindingParameters = this.oBindingParameters,
+				oView = this.getView();
+
+			oChart = oView.byId("sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545");
+			oChart.bindData(oBindingParameters['sap_IconTabBar_Page_0-zihzmhk4t25xs336tb6m5jpw3_S3-m4vku79518mibul6zuclpap04_S4-4zyw24a851ybvi14fhxl2ssx5_S5-content-sap_chart_ColumnChart-1543840504545']);
+
+		}
+	});
+}, /* bExport= */ true);
---
+++ webapp/controller/EditEmployee.js
@@ -0,0 +1,134 @@
+sap.ui.define([
+	"sap/ui/base/ManagedObject",
+	"sap/m/MessageBox",
+	"./utilities",
+	"sap/ui/core/routing/History"
+], function(ManagedObject, MessageBox, Utilities, History) {
+
+	return ManagedObject.extend("com.sap.build.standard.project3.controller.EditEmployee", {
+		constructor: function(oView) {
+			this._oView = oView;
+			this._oControl = sap.ui.xmlfragment(oView.getId(), "com.sap.build.standard.project3.view.EditEmployee", this);
+			this._bInit = false;
+		},
+
+		exit: function() {
+			delete this._oView;
+		},
+
+		getView: function() {
+			return this._oView;
+		},
+
+		getControl: function() {
+			return this._oControl;
+		},
+
+		getOwnerComponent: function() {
+			return this._oView.getController().getOwnerComponent();
+		},
+
+		open: function() {
+			var oView = this._oView;
+			var oControl = this._oControl;
+
+			if (!this._bInit) {
+
+				// Initialize our fragment
+				this.onInit();
+
+				this._bInit = true;
+
+				// connect fragment to the root view of this component (models, lifecycle)
+				oView.addDependent(oControl);
+			}
+
+			var args = Array.prototype.slice.call(arguments);
+			if (oControl.open) {
+				oControl.open.apply(oControl, args);
+			} else if (oControl.openBy) {
+				oControl.openBy.apply(oControl, args);
+			}
+		},
+
+		close: function() {
+			this._oControl.close();
+		},
+
+		setRouter: function(oRouter) {
+			this.oRouter = oRouter;
+
+		},
+		getBindingParameters: function() {
+			return {};
+
+		},
+		_onButtonPress: function() {
+			var oView = this.getView();
+			var oController = this;
+
+			return new Promise(function(fnResolve, fnReject) {
+				var oModel = oController.oModel;
+
+				var fnResetChangesAndReject = function(sMessage) {
+					oModel.resetChanges();
+					fnReject(new Error(sMessage));
+				};
+				if (oModel && oModel.hasPendingChanges()) {
+					oModel.submitChanges({
+						success: function(oResponse) {
+							var oBatchResponse = oResponse.__batchResponses[0];
+							var oChangeResponse = oBatchResponse.__changeResponses && oBatchResponse.__changeResponses[0];
+							if (oChangeResponse && oChangeResponse.data) {
+								var sNewContext = oModel.getKey(oChangeResponse.data);
+								oView.unbindObject();
+								oView.bindObject({
+									path: "/" + sNewContext
+								});
+								if (window.history && window.history.replaceState) {
+									window.history.replaceState(undefined, undefined, window.location.hash.replace(encodeURIComponent(oController.sContext), encodeURIComponent(sNewContext)));
+								}
+								oModel.refresh();
+								fnResolve();
+							} else if (oChangeResponse && oChangeResponse.response) {
+								fnResetChangesAndReject(oChangeResponse.message);
+							} else if (!oChangeResponse && oBatchResponse.response) {
+								fnResetChangesAndReject(oBatchResponse.message);
+							} else {
+								oModel.refresh();
+								fnResolve();
+							}
+						},
+						error: function(oError) {
+							fnReject(new Error(oError.message));
+						}
+					});
+				} else {
+					fnResolve();
+				}
+			}).catch(function(err) {
+				if (err !== undefined) {
+					MessageBox.error(err.message);
+				}
+			});
+
+		},
+		_onButtonPress1: function() {
+
+			this.close();
+
+		},
+		onInit: function() {
+
+			this._oDialog = this.getControl();
+
+			this.oModel = this.getOwnerComponent().getModel();
+
+		},
+		onExit: function() {
+			this._oDialog.destroy();
+
+		}
+
+	});
+}, /* bExport= */ true);
